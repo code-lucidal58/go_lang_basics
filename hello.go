fcd32966ea5ddd6b95d3f02be4d5d12595800e38
Aanisha Mishra <aanisha.mishra05@gmail.com>
2018-04-29 23:55:53 +0530

    added comments with code

--- hello.go
+++ hello.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main()  {
+	//declaring a variable and assigning it later
 	var a string
 	a = "Aanisha"
+	//declaring a variable as well as assigning it simultaneously
 	b := 6
+	//printing a line by concatenating strings and integers
 	fmt.Println("Hello World!",a, b)
+
+	//declaring a constant and printing it
 	const y = "y is constant"
 	fmt.Println(y)
 }
