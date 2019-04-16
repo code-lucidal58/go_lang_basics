44850f43f7ce3ab57d011c717efbea9763a0b669
code-lucidal58 <aanisha.mishra05@gmail.com>
2019-04-16 13:23:12 +0530

    cards project in progress

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,18 @@ import "fmt"
 // which is a slice of string
 
 type deck []string // deck borrows all features of slice string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
